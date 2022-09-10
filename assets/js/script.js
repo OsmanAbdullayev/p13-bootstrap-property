4954e9fe46df3d40dbf61f17c419061de24619ce
Osman <os.abdullayev@gmail.com>
2022-09-11 01:15:23 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const a = prompt();
+
+if (a == 1) {
+	console.log("a beraberdir 1");
+} else if (a == 2) {
+	console.log("a beraberdir 2");
+} else if (a == 3) {
+	console.log("a beraberdir 3");
+} else {
+	console.log("Duz reqemden istifade edin");
+}
